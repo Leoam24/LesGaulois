9b3af42a205877e97efd0d51fa6ecf40bfde0174
Leo_A_M <leoahmedmushtaq@gmail.com>
2025-01-22 09:17:44 +0100

    TP2 part3 Quest d)

--- src/sites/Camp.java
+++ src/sites/Camp.java
@@ -4,15 +4,25 @@ import personnages.Romain;
 
 public class Camp {
 	private Soldat commandant;
+	private int nbPersonneMax = 80;
+	private Soldat[] soldats = new Soldat[nbPersonneMax];
+	private int nbSoldats;
 	
 	public Camp(Soldat minus) {
 		this.commandant = minus;
-		
+		this.nbSoldats++;
 	}
 	
 	public Soldat getCommandant() {
 		return commandant;
 	}
 	
+	public void ajouterSoldat(Soldat soldat) {
+		if((soldat != null) && ( nbSoldats < nbPersonneMax ) ) {
+			soldats[nbSoldats] = soldat;
+			nbPersonneMax++;
+		}
+		
+	}
 	
 }
--- src/sites/Village.java
+++ src/sites/Village.java
@@ -2,13 +2,30 @@ package sites;
 import personnages.Gaulois;
 
 public class Village {
-	public Gaulois chef;
+	private Gaulois chef;
+    private int nbVillageoisMax = 50;
+	private Gaulois[] villageois = new Gaulois[nbVillageoisMax];
+    private int nbVillageois;
+	
 	
 	public Village(Gaulois chef) {
 		this.chef = chef;
+		this.nbVillageois++;
 	}
 	
 	public Gaulois getChef() {
 		return chef;
 	}
+
+	public void ajouterVillageois(Gaulois gaulois) {
+		if((gaulois != null) && ( nbVillageois < nbVillageoisMax ) ) {
+			nbVillageoisMax++;
+			villageois[nbVillageoisMax] = gaulois;
+			
+		}
+		
+	}
+
+
+
 }
